a430d51743f2c56428f17e760716450a24b50de8
toshikase <yomihitoshirazu@outlook.com>
2015-09-22 15:17:58 +0900

    Initial Commit

---
+++ Realart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Realart
+//
+//  Created by 敏宏 加世田 on 9/22/15.
+//  Copyright (c) 2015 Sendee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
