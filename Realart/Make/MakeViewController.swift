005b51184150ee3626e536206460d7735441dead
t.kaseda <toshihirokaseda@toshihiros-MacBook-Pro.local>
2015-10-08 12:49:32 +0900

    add qrcode read fun.

--- Realart/Make/MakeViewController.swift
+++ Realart/Make/MakeViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CoreImage
 
-class MakeViewController: UIViewController {
+class MakeViewController: UIViewController, UITextFieldDelegate{
     @IBOutlet weak var postButton: UIButton!
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var titleField: UITextField!
@@ -19,6 +20,45 @@ class MakeViewController: UIViewController {
         super.viewDidLoad()
         postButton.setTitle("投稿", forState:.Normal)
         registerButton.setTitle("登録", forState: .Normal)
+        
+        nameField.delegate = self
+    }
+    
+    func createQRCode(message: String, correctionLevel: String = "M", moduleSize: CGFloat = 1) -> UIImage {
+        
+        let dat = message.dataUsingEncoding(NSUTF8StringEncoding)!
+        
+        let qr = CIFilter(name: "CIQRCodeGenerator", withInputParameters: [
+            "inputMessage": dat,
+            "inputCorrectionLevel": correctionLevel,
+            ])!
+        
+        // moduleSize でリサイズ
+        let sizeTransform = CGAffineTransformMakeScale(moduleSize, moduleSize)
+        let ciImg = qr.outputImage!.imageByApplyingTransform(sizeTransform)
+
+        print(ciImg)
+        return UIImage(CIImage: ciImg, scale: 1, orientation: .Up)
+//        let ciContext = CIContext(options: nil)
+//        let cgImg = ciContext.createCGImage(ciImg, fromRect: ciImg.extent)
+//        return UIImage(CGImage: cgImg, scale: 1, orientation: .Up)
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField){
+        print("textFieldDidBeginEditing:" + textField.text!)
+    }
+    
+    func textFieldShouldEndEditing(textField: UITextField) -> Bool {
+        print("textFieldShouldEndEditing:" + textField.text!)
+        
+        return true
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        createQRCode(textField.text!)
+        
+        return true
     }
 
     override func didReceiveMemoryWarning() {
--- Realart/Read/ReadViewController.swift
+++ Realart/Read/ReadViewController.swift
@@ -7,11 +7,79 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ReadViewController: UIViewController {
+class ReadViewController: UIViewController,AVCaptureMetadataOutputObjectsDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.whiteColor()
+        
+        // セッションの作成.
+        let mySession: AVCaptureSession! = AVCaptureSession()
+        
+        // デバイス一覧の取得.
+        let devices = AVCaptureDevice.devices()
+        
+        // デバイスを格納する.
+        var myDevice: AVCaptureDevice!
+        
+        // バックカメラをmyDeviceに格納.
+        for device in devices{
+            if(device.position == AVCaptureDevicePosition.Back){
+                myDevice = device as! AVCaptureDevice
+            }
+        }
+        
+        // バックカメラから入力(Input)を取得.
+        do {
+            let myVideoInput = try AVCaptureDeviceInput(device: myDevice) as AVCaptureDeviceInput
+            // moved the rest of the image capture into the do{} scope.
+            
+            if mySession.canAddInput(myVideoInput) {
+                // セッションに追加.
+                mySession.addInput(myVideoInput)
+            }
+            
+        } catch let error as NSError {
+            // Handle any errors
+            print(error)
+        }
+        
+        
+        // 出力(Output)をMeta情報に.
+        let myMetadataOutput: AVCaptureMetadataOutput! = AVCaptureMetadataOutput()
+        
+        if mySession.canAddOutput(myMetadataOutput) {
+            // セッションに追加.
+            mySession.addOutput(myMetadataOutput)
+            // Meta情報を取得した際のDelegateを設定.
+            myMetadataOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+            // 判定するMeta情報にQRCodeを設定.
+            myMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+        }
+        
+        // 画像を表示するレイヤーを生成.
+        let myVideoLayer : AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: mySession) as AVCaptureVideoPreviewLayer
+        myVideoLayer.frame = self.view.bounds
+        myVideoLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        
+        // Viewに追加.
+        self.view.layer.addSublayer(myVideoLayer)
+        
+        // セッション開始.
+        mySession.startRunning()
+    }
+    
+    // Meta情報を検出際に呼ばれるdelegate.
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+        if metadataObjects.count > 0 {
+            let qrData: AVMetadataMachineReadableCodeObject  = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+            print("\(qrData.type)")
+            print("\(qrData.stringValue)")
+            
+            // SafariでURLを表示.
+//            UIApplication.sharedApplication().openURL(NSURL(string: qrData.stringValue)!)
+        }
     }
 
     override func didReceiveMemoryWarning() {
