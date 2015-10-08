63b9b16f2baed3d4385f3c990c1c08ef3c908eb8
t.kaseda <toshihirokaseda@toshihiros-MacBook-Pro.local>
2015-10-08 11:42:45 +0900

    added tab_picture. added listVC

---
+++ Realart/List/ListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ListViewController.swift
+//  Realart
+//
+//  Created by 敏宏 加世田 on 10/8/15.
+//  Copyright © 2015 Sendee. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
